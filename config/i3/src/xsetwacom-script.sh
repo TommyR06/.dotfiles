ecfd9e0056c4217cb77629c7bf354207854d8131
Tommaso Rizzi <tommasorizzi06@gmail.com>
2020-05-13 16:03:15 +0200

    Add setting for Wacom Bamboo1 Pen

---
+++ config/i3/src/xsetwacom-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Wacom Bamboo1 Pen Setting
+# set stylus button 3 to "Ctrl z"
+
+xsetwacom set "Wacom Bamboo1 Pen stylus" Button 3 "key +ctrl z -ctrl"
+
--- config/i3/src/xss-lock-script.sh
+++ config/i3/src/xss-lock-script.sh
@@ -6,6 +6,10 @@
 # set cycle timer after screensaver to 10 minutes 
 # exec systemctl suspend once it's fired
 
+# disable DPMS (conflict with suspension)
+xset -dpms
+# set timeout and cycle for screensaver in seconds
 xset s 300 600 
+# set launchers used by xss-lock
 xss-lock -n $HOME/.config/i3/src/i3lock-script.sh -- systemctl suspend
 
